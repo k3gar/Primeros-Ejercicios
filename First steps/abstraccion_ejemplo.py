e0f34dcb9a6f937086dab53750382e370cbf3fe9
Kevin Garcia <69099162+k3gar@users.noreply.github.com>
2021-01-12 21:35:09 -0600

    Mejoras 1.1
    Ahora permite que uno ingrese el producto que quiere comprar y que pueda especificar el estado de la caja

--- First steps/abstraccion_ejemplo.py
+++ First steps/abstraccion_ejemplo.py
@@ -1,4 +1,6 @@
 import time
+item = input('¿Que desea comprar?: ')
+capacidad = input('Si la caja se encuentra vacia escrbia ¨libre¨ \nSi la caja se encuentra llena, escriba ¨lleno¨: ')
 
 class supermercado:
     def __init__(self):
@@ -8,6 +10,7 @@ class supermercado:
         self.producto = producto
         self._pasillo()
         self._caja()
+        #self._estado()
 
     
     def _pasillo(self):
@@ -15,15 +18,31 @@ class supermercado:
         time.sleep(5.5)
         print(f'Caminando entre pasillos')
     
-    def _caja(self, estado=1):
-        while estado != 10:
-            print(f'Caja llena, espere un momento')
-            time.sleep(3)
-            estado += 1
+    def _caja(self, estado=0):
+        if capacidad == 'libre':
+            print(f'La caja esta libre, puede pasar')
+
+        elif capacidad == 'lleno':
+            while estado != 3:
+                print(f'Caja llena, espere un momento')
+                time.sleep(2)
+                estado += 1
+            else:
+                print('Compra realizada')
+                print(f'Logró comprar {item}')   
         else:
-            print('Compra realizada')
-            
+            print('Opcion incorrecta') 
+    #def _estado(self, capacidad):
+    #    self.capacidad = capacidad
+    #    if capacidad == 'libre':
+    #        print(f'La caja esta libre, puede pasar')
+        
+    #    elif capacidad != 'libre':
+    #        _caja
+    #    else:
+    #        pass
 
 if __name__ == '__main__':
     supermercado = supermercado()
-    supermercado.comprar('Atun')
+    supermercado.comprar(item)
+    #supermercado.estado(capacidad)
